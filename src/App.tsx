592062d84a4399487c7c541ba85ceb89cc0df025
ChristianCodesNothing <christiand.briggs@gmail.com>
2024-12-10 23:44:17 -0500

    connected to weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import "./App.css"
+import { WeatherData } from "./WeatherData"
 
 export const App = () => {
-  
+  const [temperature, setTemperature] = useState(0)
+  const [windSpeed, setWindSpeed] = useState(0)
+  const [windDir, setWindDir] = useState(0)
+  const [humidity, setHumidity] = useState(0)
+
+  useEffect(() => {
+    const url =
+      "https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=pQ81oWh9Fa4k5TQwyYka7wkhSQGKVLfm"
+    fetch(url)
+      .then((res) => res.json())
+      .then((weatherJson: WeatherData) => {
+        const values = weatherJson.data.values
+        setTemperature(values.temperature)
+        setWindSpeed(values.windSpeed)
+        setWindDir(values.windDirection)
+        setHumidity(values.humidity)
+      })
+  }, [])
 
   return (
-    <div className='h-5 w-5 bg-orange-600 '>
-      
+    <div className="">
+      <div className="h-56 w-56 rounded-lg border bg-stone-200">
+        <div className="text-sm">{temperature}</div>
+        <div className="text-sm">{windSpeed}</div>
+        <div className="text-sm">{windDir}</div>
+        <div className="text-sm">{humidity}</div>
+      </div>
     </div>
   )
 }
-
-
